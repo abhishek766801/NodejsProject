46456160fe0aa94899bef00c799a13aef33197e3
abhishek766801 <52329244+abhishek766801@users.noreply.github.com>
2023-07-30 09:53:42 +0000

    new commit

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const path=require('path')
-const g="g"
-const fs=require('fs')
-const http=require('http')
-const loadash=require('lodash')
-const arr=[1,[2,[3,[4]]]]
+const path = require('path')
+const g = "g";
+const fs = require('fs')
+const http = require('http')
+const loadash = require('lodash')
+const arr = [1, [2, [3, [4]]]]
 console.log(loadash.flattenDeep(arr))
 
-const func=(k)=>{
-console.log(`${k} hello`)
-  console.log(path.resolve(__dirname,"heloo","hello"))
-  fs.writeFileSync('./text.txt',"hello",{flag:'a'})
-  console.log(fs.readFileSync('./text.txt','utf8'))
-  fs.readFile('./text.txt','utf8',(err,result)=>{
+const func = (k) => {
+  console.log(`${k} hello`)
+  console.log(path.resolve(__dirname, "heloo", "hello"))
+  fs.writeFileSync('./text.txt', "hello", { flag: 'a' })
+  console.log(fs.readFileSync('./text.txt', 'utf8'))
+  fs.readFile('./text.txt', 'utf8', (err, result) => {
     console.log(result)
     console.log(err)
   })
 }
-const server=http.createServer((req,res)=>{
-   res.write('hell');
-   res.end('hello');
+const server = http.createServer((req, res) => {
+  res.write('hell');
+  res.end('hello');
 })
 server.listen(3000);
 //func("jt");
